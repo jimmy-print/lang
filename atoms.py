fbafa31e313664951b069eec0aa28efeafe2cf3b
jimmy-print <jimmy.wang123@outlook.sg>
2021-01-12 18:41:36 +0800

    change classes and sep file

---
+++ atoms.py
@@ -0,0 +1,64 @@
+LEFT, RIGHT = 'LEFT', 'RIGHT'
+
+class Int:
+    """End nodes on a tree"""
+    def __init__(self, v):
+        assert type(v) == int
+        self.v = v
+        self.r1 = None
+        self.r2 = None
+
+    def __call__(self):
+        return self.v
+
+class Function:
+    """Not end nodes"""
+    def __init__(self):
+        self.v = 'ADD'
+    def add_left(self, r1):
+        self.r1 = r1
+
+    def add_right(self, r2):
+        self.r2 = r2
+
+    def __call__(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def thru_vis(node, layer=0, side=None):
+        gap = ''
+        for _ in range(0, layer):
+            gap += ' '
+        print('%s %s' % (gap, node.v))
+        layer += 1
+        if node.r1 is not None:
+            Function.thru_vis(node.r1, layer, side=LEFT)
+        if node.r2 is not None:
+            Function.thru_vis(node.r2, layer, side=RIGHT)
+
+    def __getitem__(self, item):
+        """indexing using depth first search"""
+        for i, node in enumerate(Function.thru(self)):
+            if i == item:
+                return node
+        raise IndexError('tree index out of range')
+
+    def get_item_using_branch_path(self, path):
+        if path == []:
+            return self
+        for i in path:
+            assert i == LEFT or i == RIGHT or i is None
+
+        tmp_node = self
+        for direction in path:
+            if direction is None:
+                tmp_node = tmp_node
+            if direction == LEFT:
+                tmp_node = tmp_node.r1
+            if direction == RIGHT:
+                tmp_node = tmp_node.r2
+        return tmp_node
+
+class Adder(Function):
+    def __call__(self):
+        return self.r1() + self.r2()
--- psil.py
+++ psil.py
@@ -1,124 +1,41 @@
 #!/usr/bin/env python3
 
-LEFT, RIGHT = 'LEFT', 'RIGHT'
+import atoms
+from atoms import Function, Adder, Int
 
 
-class Node:
-    # node on tree
-    def __init__(self, v):
-        self.v = v
-        self.r1 = None
-        self.r2 = None
-
-    def add_left(self, r1):
-        self.r1 = r1
-
-    def add_right(self, r2):
-        self.r2 = r2
-
-    def __call__(self):
-        if self.v == 'A(':
-            return self.r1() + self.r2()
-        elif type(self.v) == int:
-            return self.v
-
-    @staticmethod
-    def thru(node):
-        """depth first iterate"""
-        yield node
-        if node.r1 is not None:
-            yield from Node.thru(node.r1)
-        if node.r2 is not None:
-            yield from Node.thru(node.r2)
-
-    @staticmethod
-    def thru_vis(node, layer=0, side=None):
-        gap = ''
-        for _ in range(0, layer):
-            gap += ' '
-        print('%s %s %s' % (gap, node.v, side))
-        layer += 1
-        if node.r1 is not None:
-            Node.thru_vis(node.r1, layer, side=LEFT)
-        if node.r2 is not None:
-            Node.thru_vis(node.r2, layer, side=RIGHT)
-
-    def __getitem__(self, item):
-        """indexing using depth first search"""
-        for i, node in enumerate(Node.thru(self)):
-            if i == item:
-                return node
-        raise IndexError('tree index out of range')
-
-    def get_item_using_branch_path(self, path):
-        if path == []:
-            return self
-        for i in path:
-            assert i == LEFT or i == RIGHT or i is None
-
-        tmp_node = self
-        for direction in path:
-            if direction is None:
-                tmp_node = tmp_node
-            if direction == LEFT:
-                tmp_node = tmp_node.r1
-            if direction == RIGHT:
-                tmp_node = tmp_node.r2
-        return tmp_node
-
-
-def add_node(root, index, v1, v2):
-    to_add_on = root[index]
-    assert to_add_on.r1 is None and to_add_on.r2 is None
-    to_add_on.add_left(Node(v1))
-    to_add_on.add_right(Node(v2))
-
-
-def add_node_left(root, index, v1):
-    to_add_on = root[index]
-    assert to_add_on.r1 is None
-    to_add_on.add_left(Node(v1))
-
-
-def add_node_right(root, index, v2):
-    to_add_on = root[index]
-    assert to_add_on.r2 is None
-    to_add_on.add_right(Node(v2))
-
-
-add_path = [LEFT]
+add_path = [atoms.LEFT]
 def get_add_paths_and_vals(s):
     for t in s[1:len(s)]:
-        if t == 'A(':
-            if add_path[-1] == LEFT:
+        if t == 'ADD(':
+            if add_path[-1] == atoms.LEFT:
                 yield add_path, t
-                add_path.append(LEFT)  # add_path for next t
-            elif add_path[-1] == RIGHT:
+                add_path.append(atoms.LEFT)  # add_path for next t
+            elif add_path[-1] == atoms.RIGHT:
                 yield add_path, t
-                add_path.append(LEFT)
+                add_path.append(atoms.LEFT)
         elif t == ')':
             add_path.pop()
             if len(add_path) == 0:
                 break  # if it's the last closing paren
-            if add_path[-1] == LEFT:
-                add_path[-1] = RIGHT
-            elif add_path[-1] == RIGHT:
-                add_path[-1] = RIGHT
+            if add_path[-1] == atoms.LEFT:
+                add_path[-1] = atoms.RIGHT
+            elif add_path[-1] == atoms.RIGHT:
+                add_path[-1] = atoms.RIGHT
         else:
-            if add_path[-1] == LEFT:
+            if add_path[-1] == atoms.LEFT:
                 yield add_path, t
-                add_path[-1] = RIGHT
-            elif add_path[-1] == RIGHT:
+                add_path[-1] = atoms.RIGHT
+            elif add_path[-1] == atoms.RIGHT:
                 yield add_path, t
 
 
 def get_tree(s):
-    root = Node(s[0])
+    root = Adder()
     for find_path, val in get_add_paths_and_vals(s):
         on_node_path = find_path[0:len(find_path) - 1]
         on_node = root.get_item_using_branch_path(on_node_path)
 
-        is_int = None
         try:
             int(val)
         except ValueError:
@@ -127,13 +44,14 @@ def get_tree(s):
             is_int = True
 
         direction = find_path[-1]
-        if direction == LEFT:
-            on_node.add_left(Node(int(val))) if is_int else on_node.add_left(Node(val))
-        elif direction == RIGHT:
-            on_node.add_right(Node(int(val))) if is_int else on_node.add_right(Node(val))
+        if direction == atoms.LEFT:
+            on_node.add_left(Int(int(val))) if is_int else on_node.add_left(Adder())
+        elif direction == atoms.RIGHT:
+            on_node.add_right(Int(int(val))) if is_int else on_node.add_right(Adder())
 
     return root
 
 
 tree = get_tree(input('Enter expression: ').strip().split())
-print(tree())
+Function.thru_vis(tree)
+print('Answer: %s' % tree())
