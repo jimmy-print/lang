5db5c26d834d7deea2f32c516ce349ea57a6adb5
jimmy-print <jimmy.wang123@outlook.sg>
2021-05-29 10:03:07 +0800

    reorganise files, add new functions

--- ll.py
+++ ll.py
@@ -38,6 +38,9 @@ class Thing(Node):
         return self.v
     
 class Adder(Node):
+    # the name class field is the function name.
+    # add function must thus be called by (add x y z ...)
+    name = 'add'
     def __call__(self):
         out = 0
         for node in self.nodes:
@@ -45,9 +48,29 @@ class Adder(Node):
                 out += node()
         return out
 
+class Subtracter(Node):
+    name = 'sub'
+    expected_args_len = 2
+    def __call__(self):
+        if len(self.nodes) != type(self).expected_args_len + 1:  # + 1, because the nodes include the function name as a string
+            raise RuntimeError('sub function takes exactly two arguments')
+        return self.nodes[1]() - self.nodes[2]()
+
+class Printer(Node):
+    name = 'print'
+    expected_args_len = 1
+    def __call__(self):
+        if len(self.nodes) != type(self).expected_args_len + 1:
+            raise RuntimeError('print function takes exactly 1 argument')
+        print(self.nodes[1]())
+
+class Root(Node):
+    # returns the return value of its first node. it should only have one son node.
+    def __call__(self):
+        return self.nodes[0]()
+
 NICHT = '('
 BACK = ')'
-ADD = 'ADD'
 
 def is_int(v):
     try:
@@ -56,26 +79,74 @@ def is_int(v):
     except ValueError:
         return False
 
-def get_tree(l):
-    tokens = l.split()
 
-    tree = Adder(NICHT, None)
+functions = Adder, Subtracter, Printer
+def get_appropriate_function_class(tok):
+    for function in functions:
+        if tok == function.name:
+            return function
+    raise RuntimeError(f'{tok} function not found')
 
-    on_tok_i = 0
+
+def get_tokens(s):
+    split = s.strip().split()
+    def remove_all_instances_of_right_paren(string):
+        out = []
+        for c in string:
+            if c != ')':
+                out.append(c)
+        return ''.join(out)
+
+    out = []
+
+    # Check if (add 1 1 ) or ( add 1 1) or ( add 1 1 )
+    for tok in split:
+        if tok == ')' or tok == '(':
+            raise RuntimeError('Syntax')
+
+    for tok in split:
+        if ')' not in tok:
+            out.append(tok)
+        else:
+            out.append(remove_all_instances_of_right_paren(tok))
+            len_of_right_paren = 0
+            for c in tok:
+                if c == ')':
+                    len_of_right_paren += 1
+            for _ in range(len_of_right_paren):
+                out.append(')')
+
+    outout = []
+    for tok in out:
+        if NICHT in tok:
+            outout.append(NICHT)
+            outout.append(tok[1:len(tok)])
+        else:
+            outout.append(tok)
+
+    return outout
+
+
+def get_tree(tokens):
+    tree = Root('ROOT', None)
+
     on_tok = tree.get(0)
 
-    i = -1
-    for _, tok in enumerate(tokens[1:len(tokens)]):
-        i += 1
+    II = -1
+    for i, tok in enumerate(tokens):
+        II += 1
         if tok == BACK:
             # now we change on_tok to the nicht above the on_tok
             on_tok = on_tok.parent
 
-            i -= 1
+            II -= 1
+
             continue
 
         if tok == NICHT:
-            on_tok.add(Adder(tok, None))
+            # scan the one forward tok to determine appropriate function
+            function_class = get_appropriate_function_class(tokens[i + 1])
+            on_tok.add(function_class(tok, None))
         else:
             if is_int(tok):
                 on_tok.add(Thing(int(tok), None))
@@ -83,8 +154,13 @@ def get_tree(l):
                 on_tok.add(Thing(tok, None))
 
         if tok == NICHT:
-            on_tok = tree.get(i + 1)
+            on_tok = tree.get(II + 1)
+
     return tree
 
+
 if __name__ == '__main__':
-    get_tree('( ADD ( ADD ( ADD 123 23 3 ) ( ADD 1 ( ADD 1 2 3 4 ) 4 ) 23 2 ) 8 2 1 )')()
+    s = '(print (add 2 3 4 45))'
+    tokens = get_tokens(s)
+    tree = get_tree(tokens)
+    tree()
--- test-new.py
+++ test-new.py
@@ -4,14 +4,18 @@ import unittest
 class Test(unittest.TestCase):
     def setUp(self):
         self.tests = {
-            '( ADD 1 2 )': 3,
-            '( ADD ( ADD 34 2 ) ( ADD 1 -100 ) )': -63,
-            '( ADD ( ADD ( ADD 123 23 3 ) ( ADD 1 ( ADD 1 2 3 4 ) 4 ) 23 2 ) 8 2 -1 )': 198,
+            '(add 1 2)': 3,
+            '(add (add 34 2) (add 1 -100))': -63,
+            '(add (add (add 123 23 3) (add 1 (add 1 2 3 4) 4) 23 2) 8 2 -1)': 198,
+            '(add (sub (sub 2 3) (add 4 5)) 1 2 3 (sub 3 4))': -5,
+            '(add 1 (add 2 (sub 3 4)) 2)': 4,
+
         }
 
     def test_all_lines(self):
         for expr in self.tests:
-            self.assertEqual(ll.get_tree(expr)(), self.tests[expr])
+            print(expr)
+            self.assertEqual(ll.get_tree(ll.get_tokens(expr))(), self.tests[expr])
 
 if __name__ == '__main__':
     unittest.main()
